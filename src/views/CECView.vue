6252b6f171a36baecb20502fd321f740c0253395
yazae <yazae@murena.io>
2023-05-07 22:14:04 +0200

    proposition: uniformisation
    Mettre des points en fin de phrase
    même pour les listes

--- src/views/CECView.vue
+++ src/views/CECView.vue
@@ -207,8 +207,8 @@ const store = useGlobalStore()
             Pour déposer votre demande, deux choix s'offrent à vous :
           </p>
           <ul>
-            <li>Une remise en main propre, directement au service d'état-civil ou au tribunal</li>
-            <li>Un envoi par courrier, en recommandé</li>
+            <li>Une remise en main propre, directement au service d'état-civil ou au tribunal.</li>
+            <li>Un envoi par courrier, en recommandé.</li>
           </ul>
           <p><strong>Le document de demande doit être daté et signé.</strong></p>
           <p>
