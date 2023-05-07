4e64a315d3b36407143d38b6b04820678820cf80
Agate <me@agate.blue>
2023-05-07 13:45:39 +0200

    Fixed typo in plausible configuration

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ if (import.meta.env.VITE_PLAUSIBLE_URL) {
   console.log("Plausible tracking enabled on ", import.meta.env.VITE_PLAUSIBLE_URL)
   const plausibleOptions = { 
     hashMode: true,
-    apiHost: import.meta.envVITE_PLAUSIBLE_URL,
+    apiHost: import.meta.env.VITE_PLAUSIBLE_URL,
     domain: import.meta.env.VITE_PLAUSIBLE_TRACKING_DOMAIN,
     trackLocalhost: !!import.meta.env.VITE_PLAUSIBLE_TRACK_LOCALHOST,
   }
