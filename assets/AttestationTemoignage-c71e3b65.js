ff3bbfc9e82a8a2232c16434c4582864981fda16
agateblue <me+github@agate.blue>
2023-05-08 14:59:31 +0000

    Deploying to gh-pages from @ agateblue/administrans@2b747a7f64b0b95001ddb50d85d3a6e50a821cb2 🚀

--- assets/AttestationTemoignage-c71e3b65.js
+++
@@ -1,1 +0,0 @@
-import"./index-edbbaa1a.js";const n={__name:"AttestationTemoignage",props:{data:{type:Object}},setup(e){return(t,r)=>" Courrier encore en cours de conception "}};export{n as default};
